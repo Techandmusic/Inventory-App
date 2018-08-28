95b66300f45d68d3e0b95c5f22c900664bd1bd6f
Adam <datriphopper@gmail.com>
2018-08-28 16:50:39 -0500

    Added declarations of insertData and queryData methods in MainActivity

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.inventoryapp;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void insertData() {
+        //add items to database
+    }
+
+    private Cursor queryData() {
+        //use cursor to read from database
     }
 }
 
