3b05329cec25e6052e572cc34e61256513d92330
Adam <datriphopper@gmail.com>
2018-09-12 09:47:14 -0500

    Added TODO list

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -106,3 +106,17 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
+//TODO Create "Add Inventory" Fragment
+//TODO Create "Edit Product Details" Fragment
+//TODO Create "View Product Details" Fragment
+//TODO Setup Query Method to display all invnetory in MainActivity
+//TODO Setup Update Method
+//TODO Setup Delete Method
+//TODO Setup Proper Create Method
+//TODO Setup "Sale" Button that reduces quantity of product by one
+//TODO Setup logic so that no negative quantities are displayed
+//TODO Setup Increase and Decrease qunatity buttons in product detail view
+//TODO Setup Delete Button in product detail view
+//TODO Setup Order Button in product detail view that uses a phone intent with supplier phone number
+//TODO Add Default TextView for when database is empty
+
