3a8e71c193bd68c384ee17b348f706d560f49def
Adam <datriphopper@gmail.com>
2018-09-16 16:41:55 -0500

    Added bindings to views in ViewProductDetails and EditProductDetails respectively.

--- app/src/main/java/com/example/android/inventoryapp/EditProductDetails.java
+++ app/src/main/java/com/example/android/inventoryapp/EditProductDetails.java
@@ -1,6 +1,22 @@
 package com.example.android.inventoryapp;
 
 import android.support.v4.app.Fragment;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class EditProductDetails extends Fragment {
+    //EditText field to add book title
+    @BindView(R.id.addTitle) EditText title;
+    //EditText field to add book author
+    @BindView(R.id.addAuthor) EditText author;
+    //EditText field to add book price
+    @BindView(R.id.addPrice) EditText price;
+    //EditText field to add quantity of book in stock
+    @BindView(R.id.addQuantity) EditText quantity;
+    //EditText field to add name of supplier for book
+    @BindView(R.id.addSupplierName) EditText supplierName;
+    //EditText field to add supplier's phone number
+    @BindView(R.id.addSupplierNo) EditText supplierNo;
+
 }
--- app/src/main/java/com/example/android/inventoryapp/ViewProductDetails.java
+++ app/src/main/java/com/example/android/inventoryapp/ViewProductDetails.java
@@ -1,6 +1,9 @@
 package com.example.android.inventoryapp;
 
 import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class ViewProductDetails extends Fragment {
 
@@ -8,6 +11,17 @@ public class ViewProductDetails extends Fragment {
 
     //Bind Column results from list item to views in
     //product_details.xml
+
+    //TextView for book title
+    @BindView(R.id.bookTitle) TextView bookTitle;
+    //TextView for book author
+    @BindView(R.id.bookAuthor) TextView bookAuthor;
+    //TextView for book price
+    @BindView(R.id.bookPrice) TextView bookPrice;
+    //TextView for book quantity
+    @BindView(R.id.bookQuantity) TextView bookQuantity;
+    //TextView for book supplier name
+    @BindView(R.id.bookSupplier) TextView bookSupplier;
 
 
 }
