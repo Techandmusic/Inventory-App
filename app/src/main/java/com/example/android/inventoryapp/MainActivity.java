af60596e3c2028062ea738fe30a33fe8a0719973
Adam <datriphopper@gmail.com>
2018-09-23 14:34:33 -0500

    Added FloatingActionButton in MainActivity and added Dependencies in Gradle.

--- app/src/main/java/com/example/android/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -7,19 +7,15 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.support.design.widget.FloatingActionButton;
 import com.example.android.inventoryapp.data.BookContract.BookEntry;
 import com.example.android.inventoryapp.data.BookDbHelper;
 
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     //Database helper object
     private BookDbHelper mDbHelper;
     //ListView for books
@@ -33,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup FAB to open EditProductDetails Fragment
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
 
         //Instantiate DbHelper object
         mDbHelper = new BookDbHelper(this);
