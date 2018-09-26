a7be8450ad86f17b2f332753b02c3fd39c8af9dd
Adam <datriphopper@gmail.com>
2018-09-25 22:15:43 -0500

    Updated tags in manifest. Same issue persists.

--- app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
+++ app/src/main/java/com/example/android/inventoryapp/data/BookContract.java
@@ -25,10 +25,10 @@ public final class BookContract {
         //MIME type constants
         //MIME type for a list of books
         public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_BOOKS;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
         //MIME type for a single book
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + PATH_BOOKS;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
     }
 
 
